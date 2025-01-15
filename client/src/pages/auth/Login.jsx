28c8a71338a7807166c939b1b408ac4b07517791
Ritesh Gore <rjgore136@gmail.com>
2025-01-15 15:24:25 +0530

    Final commit

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,4 +1,4 @@
-import CommonForm from "@/components/common/form";
+import CommonForm from "@/components/common/Form";
 // import { useToast } from "@/components/ui/use-toast";
 import { useToast } from "@/hooks/use-toast";
 import { loginFormControls } from "@/config";
