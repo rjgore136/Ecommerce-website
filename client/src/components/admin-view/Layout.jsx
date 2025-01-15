aaed9fc91c260eab36fc82979ca826009f2e6dde
Ritesh Gore <rjgore136@gmail.com>
2025-01-15 15:21:41 +0530

    Final commit

--- client/src/components/admin-view/Layout.jsx
+++ client/src/components/admin-view/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import AdminSideBar from "./sidebar";
-import AdminHeader from "./header";
+import AdminSideBar from "./Sidebar";
+import AdminHeader from "./Header";
 import { useState } from "react";
 
 function AdminLayout() {
